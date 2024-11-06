364d9d210a0d63b76a8eee2f6316c14a20da0933
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 12:03:39 -0500

    Update tecnologia.php

--- api/secciones/tecnologia.php
+++ api/secciones/tecnologia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Elon Musk y su red satelital",
+"autor" => "X",
+"resumen" => "Elon Musk publico que brindaria de forma gratuita su red satelital a los hondureños",
 ];
 ?>
