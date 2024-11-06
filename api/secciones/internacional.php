39813694d2f132fd8085e9973802e830239562b1
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:55:43 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -6,6 +6,6 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 $internacional = [
 "titulo" => "ELECCIONES EN USA",
 "autor" => "MarcianoNew",
-"resumen" => "OBTUBO LA MAYORIA DE VOTOS",
+"resumen" => "Donald Trump OBTUBO LA MAYORIA DE VOTOS",
 ];
 ?>
