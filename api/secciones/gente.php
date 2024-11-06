78ddc0f09361940be0dd4b2e567562a14acadef0
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:54:11 -0500

    Update gente.php

--- api/secciones/gente.php
+++ api/secciones/gente.php
@@ -1,11 +1,11 @@
-<?php
+ <?php
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $gente = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Encuentras sobrevivientes tras la DANA",
+"autor" => "Español",
+"resumen" => "Bomberos encuentran a 30 sobrevivientes",
 ];
 ?>
