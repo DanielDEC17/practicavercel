58f7dab0231c80618465c88bfb3f1cb0dc57778b
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:52:15 -0500

    Update formacion.php

--- api/secciones/formacion.php
+++ api/secciones/formacion.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $formacion = [
-"titulo" => "XXX",
-"autor" => "sistemas@uns",
-"resumen" => "XXX",
+"titulo" => "Coursera lanza cursos gratuitos de IA",
+"autor" => "Coursera",
+"resumen" => "Tras el auge de la IA Coursera ha decidido dar de forma gratuita sus cursos de IA para los 500 primeros usuarios en registrarse",
 ];
 ?>
