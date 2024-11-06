4951400625031d1896e1e743fb3101d78ce70c6f
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:32:25 -0500

    Update internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "ELECCIONES EN USA",
+"autor" => "MarcianoNew",
+"resumen" => "OBTUBO LA MAYORIA DE VOTOS",
 ];
 ?>
