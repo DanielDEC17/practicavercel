2f80aae25f316e346564c968e4cc23247ff4033f
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 12:02:09 -0500

    Update sociedad.php

--- api/secciones/sociedad.php
+++ api/secciones/sociedad.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "España se envuelve en marchas por la elevacion de impuestos",
+"autor" => "Oxfam",
+"resumen" => "Muchos españoles marchan en contra del gobierno por elevar los impuestos",
 ];
 ?>
