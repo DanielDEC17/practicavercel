d6c68e8562200b7de01e23de6f517ae35db2c5ce
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:35:36 -0500

    Update economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "TRUMP Y LAS CRYPTOS",
+"autor" => "MARIO",
+"resumen" => "La victoria de Donal Trump aumento el valor de muchas cryptomonedas",
 ];
 ?>
