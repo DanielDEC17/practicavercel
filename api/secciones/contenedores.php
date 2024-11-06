07595fed33cd84986864ae01bfea58b8ad8450d6
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:44:41 -0500

    Update contenedores.php

--- api/secciones/contenedores.php
+++ api/secciones/contenedores.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $contenedores = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Docker lanza una nueva version de su plataforma",
+"autor" => "Docker",
+"resumen" => "Docker publico en sus redes sociales el lanzamiento de su nueva version de su plataforma",
 ];
 ?>
