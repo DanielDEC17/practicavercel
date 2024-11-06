a14b2400f7ec91d124680f774df0b3040ffde7dc
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:58:18 -0500

    Update openshift.php

--- api/secciones/openshift.php
+++ api/secciones/openshift.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $openshift = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Version Victoria",
+"autor" => "Red HAt",
+"resumen" => "Lanzamiento de  la version 4.10",
 ];
 ?>
