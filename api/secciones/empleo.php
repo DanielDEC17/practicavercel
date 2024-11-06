e61a8b42069fe2691d0bae6f4be2f5ef7c7daa1c
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:50:22 -0500

    Update empleo.php

--- api/secciones/empleo.php
+++ api/secciones/empleo.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $empleo = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "La tasa de desempleo en EE.UU baja al 3.6%",
+"autor" => "Labor Statistics",
+"resumen" => "Segun reportes se de la labor Statistics hubo una reduccion a 3.6% de desempleo",
 ];
 ?>
