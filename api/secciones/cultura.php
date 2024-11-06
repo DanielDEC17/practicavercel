b11b471fa9a04e8abe0cd614478b2d538fb9991f
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:46:06 -0500

    Update cultura.php

--- api/secciones/cultura.php
+++ api/secciones/cultura.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $cultura = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "El museo celebra su 200 aniversario",
+"autor" => "El Pais",
+"resumen" => "El Museo del Prado celebra su aniversario numero 200 con una exposicion especial",
 ];
 ?>
