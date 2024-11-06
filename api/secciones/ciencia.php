34b6270ef262294d304a7c113b0aa8eff9be9f74
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2024-11-06 11:43:30 -0500

    Update ciencia.php

--- api/secciones/ciencia.php
+++ api/secciones/ciencia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $ciencia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Descubren agua en Marte",
+"autor" => "NASA",
+"resumen" => "LA misison Mars 2020 de la NASA ha detectado agua en MArte",
 ];
 ?>
